954520b7d92c7e9d6dcd0fa1bce02abe01498708
Zhang Kai Yu <yeannylam@gmail.com>
2015-09-11 17:02:43 +0800

    Correct author email

--- expand-line.el
+++ expand-line.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2015  Kai Yu
 
-;; Author: Kai Yu <1@YeaMacky.local>
+;; Author: Kai Yu <yeannylam@gmail.com>
 ;; Keywords:
 
 ;; This program is free software; you can redistribute it and/or modify
